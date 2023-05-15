14117d205df12dfe3776507a203550076a6bfe71
Alejandro <alejandrofloresmtz94@gmail.com>
2023-05-15 09:46:13 -0600

    Types definition in Zustand Store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,16 +1,17 @@
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware';
 import { TaskType } from '../types/Task';
+import { Store } from '../types/Store';
 
-const store = (set:any) => ({
+const store = (set:Function) => ({
     tasks: [],
     addTask: (id:string, title:string, state:string) => 
-        set((store:any) => ({tasks: [...store.tasks, {id, title, state}] }), false, "addTask"),
+        set((store:Store) => ({tasks: [...store.tasks, {id, title, state}] }), false, "addTask"),
     deleteTask: (id:string) => 
-        set((store:any) => ({tasks: store.tasks.filter((task:any) => task.id !== id )}), false, "deleteTask"),
+        set((store:Store) => ({tasks: store.tasks.filter((task:TaskType) => task.id !== id )}), false, "deleteTask"),
     draggedTask: {},
-    setDraggedTask: (id:string) => set((store:any) => ( id !== "" ? { draggedTask: store.tasks.filter((task:any) => task.id === id )[0]} : {}), false, "setDraggedTask"),
-    moveTask: (oldTask:TaskType, newState:string) => set((store:any) => ({
+    setDraggedTask: (id:string) => set((store:Store) => ( id !== "" ? { draggedTask: store.tasks.filter((task:TaskType) => task.id === id )[0]} : {}), false, "setDraggedTask"),
+    moveTask: (oldTask:TaskType, newState:string) => set((store:Store) => ({
         tasks: store.tasks.map((task:TaskType) =>
             task.id === oldTask.id ? { ...oldTask, state: newState } : task
         )
@@ -19,8 +20,8 @@ const store = (set:any) => ({
     setCopySelected: ((SelectedOption:boolean) => set({ isCopySelected: SelectedOption }))
 });
 
-const log = (config:any) => (set:any, get:any, api:any) => config(
-    (...args:any[]) => {
+const log = (config:Function) => (set:Function, get:Function, api:Object) => config(
+    (...args:Object[]) => {
         //const current = get();
         // if(!current) {
             // get state from external source
@@ -32,6 +33,6 @@ const log = (config:any) => (set:any, get:any, api:any) => config(
     api
 )
 
-export const useStore = create(log(persist(devtools(store), {
+export const useStore = create<Store>(log(persist(devtools(store), {
     name: "store"
 })));
--- src/types/Store.ts
+++ src/types/Store.ts
@@ -2,11 +2,11 @@ import { TaskType } from "./Task"
 
 export type Store = {
     tasks: TaskType[],
-    addTask: (id:string, title:string, state:string) => any,
-    deleteTask: (id:string) => any,
+    addTask: (id:string, title:string, state:string) => void,
+    deleteTask: (id:string) => void,
     draggedTask: TaskType,
     setDraggedTask: (id:string) => void,
-    moveTask: (draggedTask:TaskType, state:string) => any,
+    moveTask: (draggedTask:TaskType, state:string) => void,
     isCopySelected: boolean,
-    setCopySelected: (SelectedOption:boolean) => any
+    setCopySelected: (SelectedOption:boolean) => void
 }
